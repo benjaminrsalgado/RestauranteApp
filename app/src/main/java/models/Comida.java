de60fec0bb1beaf3b6ebbb1b29e4451f1628f4ce
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-14 13:28:06 -0600

    i add two file one that show the food

---
+++ app/src/main/java/models/Comida.java
@@ -0,0 +1,19 @@
+package com.benja.restauranteapp.models;
+
+public class Comida {
+    private String nombre;
+    private double precio;
+
+    public Comida(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+}

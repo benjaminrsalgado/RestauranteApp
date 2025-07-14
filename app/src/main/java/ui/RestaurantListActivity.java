c65e2a5722d125fe086ccee3d54003d75b396a57
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-14 11:45:47 -0600

    i add xml file code

---
+++ app/src/main/java/ui/RestaurantListActivity.java
@@ -0,0 +1,13 @@
+package com.benja.restauranteapp.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.benja.restauranteapp.R;
+
+public class RestaurantListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+    }
+}

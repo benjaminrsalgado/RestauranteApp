4ac972b4a766ec6416f24ede80e902ba51f3443b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-14 12:51:32 -0600

    i change some code in order to show the app in the emulator and i create a file

--- app/src/main/java/ui/RestaurantListActivity.java
+++ app/src/main/java/ui/RestaurantListActivity.java
@@ -1,13 +1,30 @@
 package com.benja.restauranteapp.ui;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.benja.restauranteapp.R;
 
 public class RestaurantListActivity extends AppCompatActivity {
+
+    String[] restaurantes = {"Restaurante A", "Restaurante B", "Restaurante C", "Restaurante D"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+
+        ListView listView = findViewById(R.id.restaurantListView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                restaurantes
+        );
+
+        listView.setAdapter(adapter);
     }
 }
